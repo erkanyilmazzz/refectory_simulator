d43f2686830195df58d4d69e3dcbb19458d4c9be
Erkan Yılmaz <33496216+erkanyilmazzz@users.noreply.github.com>
2020-05-03 04:47:31 -0700

    Add files via upload

--- program.c
+++ program.c
@@ -22,6 +22,7 @@
 *   C is main course
 *   D is desert
 */
+
 int main(int argc, char **argv)
 {
     int *n = mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANON, -1, 0);
@@ -29,18 +30,19 @@ int main(int argc, char **argv)
     int *t = mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANON, -1, 0);
     int *s = mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANON, -1, 0);
     int *k = mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANON, -1, 0);
-
+    ////////////////////////////////////////////////////////////////////////////////////////////
     sem_t *mutex = mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANON, -1, 0);
     sem_init(mutex, 1, 1);
-
+    ////////////////////////////////////////////////////////////////////////////////////////////
     int l;
-
     argHandler(argc, argv, n, m, t, s, &l, k);
     printf("N:%d M:%d T:%d S:%d L:%d K:%d\n", *n, *m, *t, *s, l, *k);
-
+    ////////////////////////////////////////////////////////////////////////////////////////////
     int process;
     for (size_t i = 0; i < (*m); i++)
     {
+        int *student_id = mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANON, -1, 0);
+        *student_id = i;
         process = fork();
         if (process == 0)
         {
@@ -49,28 +51,31 @@ int main(int argc, char **argv)
 
                 sem_wait(mutex);
                 //student prpocess
-                printf("it is student pid is:%d and l is %d\n", getpid(), l);
+                printf("it is student pid is:%d and l is %d\n", *student_id, l);
                 l--;
 
                 sem_post(mutex);
             }
-
             exit(0);
         }
+        munmap(student_id, sizeof(int));
     }
-
+    //////////////////////////////////////////////////////////////////////////////////////////////
     for (size_t i = 0; i < *n; i++)
     {
+        int *cook_id = mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANON, -1, 0);
+        *cook_id = i;
         process = fork();
         if (process == 0)
         {
 
             //cook process
-            printf("it is cook pid is:%d\n", getpid());
+            printf("it is cook pid is:%d\n", *cook_id);
             exit(0);
         }
+        munmap(cook_id, sizeof(int));
     }
-
+    ///////////////////////////////////////////////////////////////////////////////////////////////
     for (size_t i = 0; i < (*m); i++)
     {
         wait(NULL);
